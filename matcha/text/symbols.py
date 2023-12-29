e970c76b6169c6df97a6dfce93a923328c2f23be
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-29 15:24:30 +0600

    change language cleaner

--- matcha/text/symbols.py
+++ matcha/text/symbols.py
@@ -4,16 +4,16 @@ Defines the set of symbols used in text input to the model.
 """
 # _pad = "_"
 # _punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюя"
+# _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 _letters_ipa = (
     "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 )
 
 _pad = '_'
-_punctuation = ';:,.!?¡¿—…"«»“”'
+_punctuation = ';:,.!?¡¿—…"«»“” '
 _special = '-'
 
-# _letters = 'абвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюяe'
+_letters = 'абвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюяe'
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
