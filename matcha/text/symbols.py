4e6fb5428d00770aedf4f2369f3565780dc689a9
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-29 15:50:02 +0600

    change language cleaner

--- matcha/text/symbols.py
+++ matcha/text/symbols.py
@@ -2,23 +2,17 @@
 
 Defines the set of symbols used in text input to the model.
 """
-# _pad = "_"
-# _punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюя"
+_pad = "_"
+_punctuation = ';:,.!?¡¿—…"«»“” '
+_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 _letters_ipa = (
     "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 )
 
-_pad = '_'
-_punctuation = ';:,.!?¡¿—…"«»“”[]()– '
-_special = '-',
-_numbers = '0123456789'
-
-# _letters = 'абвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюяe'
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + list(_numbers) + list(_special)
-# symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters)
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
