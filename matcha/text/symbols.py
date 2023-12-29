6419ed832e2a3b5b35718c38858eca94c6b6e690
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-29 15:43:41 +0600

    change language cleaner

--- matcha/text/symbols.py
+++ matcha/text/symbols.py
@@ -10,13 +10,14 @@ _letters_ipa = (
 )
 
 _pad = '_'
-_punctuation = ';:,.!?¡¿—…"«»“”[]()–- '
-_special = '-'
+_punctuation = ';:,.!?¡¿—…"«»“”[]()– '
+_special = '-',
+_numbers = '0123456789'
 
 # _letters = 'абвгдеёжзийклмнңоөпрстуүфхцчшщьыъэюяe'
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + list(_numbers) + list(_special)
 # symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters)
 
 # Special symbol ids
