d4c52ca3b99b44446627ccf96ed1077f100503d1
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-29 15:18:33 +0600

    change language cleaner

--- matcha/text/__init__.py
+++ matcha/text/__init__.py
@@ -18,7 +18,6 @@ def text_to_sequence(text, cleaner_names):
     sequence = []
 
     clean_text = _clean_text(text, cleaner_names)
-    print(clean_text)
     for symbol in clean_text:
         symbol_id = _symbol_to_id[symbol]
         sequence += [symbol_id]
